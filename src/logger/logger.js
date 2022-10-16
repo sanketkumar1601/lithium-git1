829d97009b8c1a8558ec48604c026fffd1272409
sanketkumar1601 <sanketkumar1601@gmail.com>
2022-10-16 23:05:58 +0530

    added

---
+++ src/logger/logger.js
@@ -0,0 +1,9 @@
+
+let welcome = function() {
+    console.log("Welcome to my application. I am sanket kumar verma and a part of FunctionUp Lithium cohort.")
+    return "Completed"
+}
+
+
+
+module.exports.myFunction = welcome
